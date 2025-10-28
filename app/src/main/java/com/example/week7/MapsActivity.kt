fbc735681e2b9ddb0031ac7a76063201c6c0749f
IlmanKhoir <muhammadfadhililmankhoir@gmail.com>
2025-10-28 17:47:43 +0700

    pinpoint current user location

--- app/src/main/java/com/example/week7/MapsActivity.kt
+++ app/src/main/java/com/example/week7/MapsActivity.kt
@@ -10,17 +10,16 @@ import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
 
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
 import com.example.week7.databinding.ActivityMapsBinding
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+import org.osmdroid.config.Configuration
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.overlay.Marker
 
-    private lateinit var mMap: GoogleMap
+
+class MapsActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityMapsBinding
     //This is the variable through which we will launch the permission request and track user responses
     private lateinit var requestPermissionLauncher: ActivityResultLauncher<String>
@@ -28,18 +27,32 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+    // important: set osmdroid user agent before loading tiles
+    // Use app-specific SharedPreferences instead of androidx.preference.PreferenceManager
+    Configuration.getInstance().load(applicationContext, applicationContext.getSharedPreferences("osmdroid", MODE_PRIVATE))
+        Configuration.getInstance().userAgentValue = packageName
+
         binding = ActivityMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        val mapFragment = supportFragmentManager
-            .findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this)
+        // configure map view
+        val map = binding.map
+        map.setTileSource(TileSourceFactory.MAPNIK)
+        map.setMultiTouchControls(true)
 
-        //This is used to register for activity result
-        //The activity result will be used to handle the permission request to the user
-        //It accepts an ActivityResultContract as a parameter
-        //which in this case we're using the RequestPermission() ActivityResultContract
+        // ini titik lokasi Griya Parungpanjang
+        val startPoint = GeoPoint(-6.3, 106.5)
+        val mapController = map.controller
+        mapController.setZoom(10.0)
+        mapController.setCenter(startPoint)
+
+        val marker = Marker(map)
+        marker.position = startPoint
+        marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
+        marker.title = "Marker in Sydney"
+        map.overlays.add(marker)
+
+        // register permission launcher
         requestPermissionLauncher =
             registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
                 if (isGranted) {
@@ -47,7 +60,6 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                     getLastLocation()
                 } else {
                     //If not granted, show a rationale dialog
-                    //A rationale dialog is used for a warning to the user that the app will now work without the required permission
                     showPermissionRationale {
                         requestPermissionLauncher.launch(ACCESS_FINE_LOCATION)
                     }
@@ -55,32 +67,16 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             }
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
-        //OnMapReady is called when the map is ready to be used
-        //The code below is used to check for the location permission for the map functionality to work
-        //If it's not granted yet, then the rationale dialog will be brought up
-        when {
-            hasLocationPermission() -> getLastLocation()
-            //shouldShowRequestPermissionRationale automatically checks if the user has denied the permission before
-            //If it has, then the rationale dialog will be brought up
-            shouldShowRequestPermissionRationale(ACCESS_FINE_LOCATION) -> {
-                showPermissionRationale {
-                    requestPermissionLauncher.launch(ACCESS_FINE_LOCATION)
-                }
-            }
-            else -> requestPermissionLauncher.launch(ACCESS_FINE_LOCATION)
-        }
+    override fun onResume() {
+        super.onResume()
+        binding.map.onResume()
     }
+
+    override fun onPause() {
+        super.onPause()
+        binding.map.onPause()
+    }
+
     //This is used to check if the user already has the permission granted
     private fun hasLocationPermission() =
         ContextCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
@@ -100,5 +96,6 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private fun getLastLocation() {
         Log.d("MapsActivity", "getLastLocation() called.")
+        // TODO: integrate location provider (FusedLocationProviderClient or Android LocationManager)
     }
 }
